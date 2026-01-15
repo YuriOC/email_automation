bbe7ece55baca4c7de87cde55aacf76b73813ad7
YuriOC <yuriuinke@gmail.com>
2026-01-15 19:04:32 -0300

    Add main entry point to app.py and ensure proper formatting

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, render_template
 from helpers import EmptyFileError, FileReadError, email_classification_AI, read_file
 
@@ -33,3 +34,7 @@ def index():
 @app.route("/result", methods=["GET"])
 def return_page():
     return render_template("result.html")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
